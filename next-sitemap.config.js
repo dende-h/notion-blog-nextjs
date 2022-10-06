bee8ab937fcbd6d5b51ebe0300b15d2f0f2b85ef
Hiroya Shitogiden <dende.capo@gmail.com>
2022-10-06 13:24:56 +0900

    sitemapURL修正

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: 'https://tech-blog-efcg.vercel.app/',
+    siteUrl: 'https://notion-novel-blog-dende.vercel.app/',
     generateRobotsTxt: true,
     sitemapSize: 7000,
   };
